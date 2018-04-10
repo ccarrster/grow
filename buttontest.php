4ff0eaada3d92527c5735efa1fdcb3f7e2753e78
ccarr <ccarrster@gmail.com>
2018-04-10 08:24:55 -0400

    tests

--- buttontest.php
+++ buttontest.php
@@ -27,7 +27,7 @@
 			var innerY = y + ((height - height * innerMultiplier) / 2.0);
 			var innerX = x + ((width - width * innerMultiplier) / 2.0);
 			for(i = -60; i <= 60; i += 60){
-				result += '<div class="hex outerhex" style="background-color: black; position: absolute; top:'+y+'px; left:'+x+'px; transform: rotate('+i+'deg); height: '+height+'px; width: '+width+'px;" onclick="'+callback+'(\''+text+'\');"></div>';
+				result += '<div class="hex outerhex" style="opacity: 0.5; background-color: '+color+'; position: absolute; top:'+y+'px; left:'+x+'px; transform: rotate('+i+'deg); height: '+height+'px; width: '+width+'px;" onclick="'+callback+'(\''+text+'\');"></div>';
 			}
 			for(i = -60; i <= 60; i += 60){
 				result += '<div class="hex innerhex" style="background-color: '+color+'; position: absolute; top:'+innerY+'px; left:'+innerX+'px; transform: rotate('+i+'deg); height: '+innerHeight+'px; width: '+innerWidth+'px;" onclick="'+callback+'(\''+text+'\');"></div>';
@@ -40,9 +40,9 @@
 <div id="grid" style="position: relative;">
 </div>
 <script>
-	document.getElementById("grid").innerHTML += makeButton(30, 10, 'logIt', 0, 'red', 'Banana');
-	document.getElementById("grid").innerHTML += makeButton(30, 110, 'logIt', 1, 'blue', 'Meat');
-	document.getElementById("grid").innerHTML += makeButton(116, 60, 'logIt', 2, 'green', 'Sandwich');
+	document.getElementById("grid").innerHTML += makeButton(30, 10, 'logIt', 0, '#ff0000', 'Banana');
+	document.getElementById("grid").innerHTML += makeButton(30, 110, 'logIt', 1, '#00ff00', 'Meat');
+	document.getElementById("grid").innerHTML += makeButton(116, 60, 'logIt', 2, '#0000ff', 'Sandwich');
 </script>
 </body>
 </html>
